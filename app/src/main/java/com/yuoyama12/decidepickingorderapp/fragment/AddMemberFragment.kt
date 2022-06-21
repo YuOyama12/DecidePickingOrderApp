d6fa22319a853bf1641736e33198beaaab8b6f22
YuOyama12 <yuoyama.5@gmail.com>
2022-06-21 17:03:45 +0900

    [Fix]View生成速度改善のためレイアウトを変更

--- app/src/main/java/com/yuoyama12/decidepickingorderapp/fragment/AddMemberFragment.kt
+++ app/src/main/java/com/yuoyama12/decidepickingorderapp/fragment/AddMemberFragment.kt
@@ -1,10 +1,10 @@
 package com.yuoyama12.decidepickingorderapp.fragment
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.yuoyama12.decidepickingorderapp.R
 import com.yuoyama12.decidepickingorderapp.databinding.FragmentAddMemberBinding
 
 class AddMemberFragment : Fragment() {
@@ -12,13 +12,33 @@ class AddMemberFragment : Fragment() {
     private var _binding : FragmentAddMemberBinding? = null
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+
+        val actionBar = activity?.findViewById<androidx.appcompat.widget.Toolbar>(R.id.action_bar)
+        actionBar?.title = getString(R.string.add_member_action_bar_title)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         _binding = FragmentAddMemberBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_add_member, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        findNavController().navigate(R.id.action_addMemberFragment_to_listFragment)
+        return super.onOptionsItemSelected(item)
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
--- app/src/main/java/com/yuoyama12/decidepickingorderapp/fragment/ListFragment.kt
+++ app/src/main/java/com/yuoyama12/decidepickingorderapp/fragment/ListFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.yuoyama12.decidepickingorderapp.R
 import com.yuoyama12.decidepickingorderapp.databinding.FragmentListBinding
 
@@ -14,9 +13,16 @@ class ListFragment : Fragment() {
     private var _binding : FragmentListBinding? = null
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val actionBar = activity?.findViewById<androidx.appcompat.widget.Toolbar>(R.id.action_bar)
+        actionBar?.title = getString(R.string.list_action_bar_title)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View {
         _binding = FragmentListBinding.inflate(inflater,container,false)
         return binding.root
