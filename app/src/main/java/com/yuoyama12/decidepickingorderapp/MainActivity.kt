7e651a853a86955ff6e94617f66250f129f9f8af
YuOyama12 <yuoyama.5@gmail.com>
2025-07-07 21:46:07 +0900

    [Add]Android15でのedge-to-edge対応のためpadding処理を追加

--- app/src/main/java/com/yuoyama12/decidepickingorderapp/MainActivity.kt
+++ app/src/main/java/com/yuoyama12/decidepickingorderapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.yuoyama12.decidepickingorderapp
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.yuoyama12.decidepickingorderapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,9 +17,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.actionBar)
+
+        // edge-to-edge対応のため、画面全体にpaddingを設ける。
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            insets
+        }
     }
 
     companion object {
